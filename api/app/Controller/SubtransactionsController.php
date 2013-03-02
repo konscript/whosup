c79adece9352014e7e57228179ab4fef6c8482f6
Søren Louv <sorenlouv@gmail.com>
2013-03-02 16:33:29 +0100

    Added handlers for transactions

--- api/app/Controller/SubtransactionsController.php
+++ api/app/Controller/SubtransactionsController.php
@@ -7,99 +7,87 @@ App::uses('AppController', 'Controller');
  */
 class SubtransactionsController extends AppController {
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->Subtransaction->recursive = 0;
-		$this->set('subtransactions', $this->paginate());
-	}
+    public function index() {
+        $subtransactions = $this->Subtransaction->find('all');
+        $this->set(array(
+            'subtransactions' => $subtransactions,
+            '_serialize' => array('subtransactions')
+        ));
+    }
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
 	public function view($id = null) {
 		if (!$this->Subtransaction->exists($id)) {
 			throw new NotFoundException(__('Invalid subtransaction'));
 		}
 		$options = array('conditions' => array('Subtransaction.' . $this->Subtransaction->primaryKey => $id));
-		$this->set('subtransaction', $this->Subtransaction->find('first', $options));
+		$subtransaction = $this->Subtransaction->find('first', $options);
+        $this->set(array(
+            'subtransaction' => $subtransaction,
+            '_serialize' => array('subtransaction')
+        ));
 	}
 
-/**
- * add method
- *
- * @return void
- */
+
 	public function add() {
+		// avoid overwriting existing
+		unset($this->request->data["id"]);
+
 		if ($this->request->is('post')) {
 			$this->Subtransaction->create();
 			if ($this->Subtransaction->save($this->request->data)) {
-				$this->Session->setFlash(__('The subtransaction has been saved'));
-				$this->redirect(array('action' => 'index'));
+				$success = true;
 			} else {
-				$this->Session->setFlash(__('The subtransaction could not be saved. Please, try again.'));
+				$success = false;
 			}
+
+			$invalidFields = $this->Subtransaction->invalidFields();
+
+	        $this->set(array(
+	            'invalidFields' => $invalidFields,
+	            'success' => $success,
+	            '_serialize' => array('invalidFields', 'success')
+	        ));
 		}
-		$payers = $this->Subtransaction->Payer->find('list');
-		$borrowers = $this->Subtransaction->Borrower->find('list');
-		$transactions = $this->Subtransaction->Transaction->find('list');
-		$this->set(compact('payers', 'borrowers', 'transactions'));
 	}
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
 	public function edit($id = null) {
 		if (!$this->Subtransaction->exists($id)) {
 			throw new NotFoundException(__('Invalid subtransaction'));
 		}
 		if ($this->request->is('post') || $this->request->is('put')) {
 			if ($this->Subtransaction->save($this->request->data)) {
-				$this->Session->setFlash(__('The subtransaction has been saved'));
-				$this->redirect(array('action' => 'index'));
+				$success = true;
 			} else {
-				$this->Session->setFlash(__('The subtransaction could not be saved. Please, try again.'));
+				$success = false;
 			}
-		} else {
-			$options = array('conditions' => array('Subtransaction.' . $this->Subtransaction->primaryKey => $id));
-			$this->request->data = $this->Subtransaction->find('first', $options);
 		}
-		$payers = $this->Subtransaction->Payer->find('list');
-		$borrowers = $this->Subtransaction->Borrower->find('list');
-		$transactions = $this->Subtransaction->Transaction->find('list');
-		$this->set(compact('payers', 'borrowers', 'transactions'));
+
+		$invalidFields = $this->Subtransaction->invalidFields();
+
+        $this->set(array(
+            'invalidFields' => $invalidFields,
+            'success' => $success,
+            '_serialize' => array('invalidFields', 'success')
+        ));
 	}
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @throws MethodNotAllowedException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		$this->Subtransaction->id = $id;
-		if (!$this->Subtransaction->exists()) {
-			throw new NotFoundException(__('Invalid subtransaction'));
-		}
-		$this->request->onlyAllow('post', 'delete');
-		if ($this->Subtransaction->delete()) {
-			$this->Session->setFlash(__('Subtransaction deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Subtransaction was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
+    public function delete($id = null) {
+        $this->Subtransaction->id = $id;
+        if (!$this->Subtransaction->exists()) {
+            throw new NotFoundException(__('Invalid subtransaction'));
+        }
+
+        $this->request->onlyAllow('post', 'delete');
+        if ($this->Subtransaction->delete()) {
+            $success = true;
+        }else {
+            $success = false;
+        }
+
+        $this->set(array(
+            'success' => $success,
+            '_serialize' => array('success')
+        ));
+    }
+
 }
