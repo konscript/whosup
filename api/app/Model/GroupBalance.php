b4800c4f4f6122de260f8c6aebdc43d7e5ce1abe
Visti Kløft <hr.kloft@gmail.com>
2013-03-02 23:51:30 +0100

    return user object as well

--- api/app/Model/GroupBalance.php
+++ api/app/Model/GroupBalance.php
@@ -5,25 +5,42 @@ App::uses('AppModel', 'Model');
  *
  */
 class GroupBalance extends AppModel {
+	
+	public $actsAs = array('Containable');
+
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
 	function getBalanceByGroupId($group_id){
 		// "SELECT user_id, SUM( balance ) AS balance
 		// 	FROM  `group_balances` 
 		// 	WHERE group_id =$group_id
 		// 	GROUP BY user_id");
-
+		$this->recursive = 2;
 		$balances = $this->find('all',array(
 				'conditions' => array('GroupBalance.group_id' => $group_id), 
 				'group' => array('GroupBalance.user_id'),
-				'fields' => array('sum(GroupBalance.balance) as balance','GroupBalance.user_id')
+				'fields' => array('sum(GroupBalance.balance) as balance','GroupBalance.user_id'),
+				// 'contain' => array('User')
 		));
 
 		$output = array();
 		foreach ($balances as $balance){
 			// debug($balance);
+			unset($balance['User']['SubtransactionPayer']);
+			unset($balance['User']['SubtransactionBorrower']);
+
 			$output[] = array(
 				'balance' => $balance[0]['balance'], 
-				'user_id' => $balance['GroupBalance']['user_id']
-				);
+				'user' => $balance['User'],
+			);
 		}
 
 		return $output;
