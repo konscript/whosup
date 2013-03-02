5d949ad7b149c809e2d58c2311757fb0bfa83900
jhh <jhh@kobstaden.dk>
2013-03-02 14:29:13 -0800

    Added jquery ui and fixed autocomplete

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -7,7 +7,20 @@ function BalancesCtrl($scope, Transactions) {
 }
 //BalancesCtrl.$inject = ['Transactions'];
 
-function NewCtrl($scope, $location, Transactions) {
+function NewCtrl($scope, $location, Transactions, facebookConnect) {
+    $scope.availableTags = [];
+    $scope.selectedUid;
+
+    facebookConnect.getFriends(function(tokens){
+        console.log(tokens);
+        $scope.availableTags = tokens.data.map(function(token){
+            return {
+                value: token.id,
+                label: token.name
+            };
+        });
+    });
+
     $scope.newTransaction = function(transaction) {
         Transactions.save(transaction);
         $location.path( "/main" );
