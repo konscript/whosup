9a9a995ad06c39269f97a18d1ca58a51f5e99f20
Visti Kløft <hr.kloft@gmail.com>
2013-03-03 14:51:20 +0100

    now saves both transaction and sub transactions in frontend

--- api/app/Controller/TransactionsController.php
+++ api/app/Controller/TransactionsController.php
@@ -46,22 +46,70 @@ class TransactionsController extends AppController {
 
 
 	public function add() {
+		
 		// avoid overwriting existing
 		unset($this->request->data["id"]);
 
 		if ($this->request->is('post')) {
+			
+
+			$this->loadModel('User');
+			$this->loadModel('Subtransaction');
+
+			$subtransactions = $this->request->data['subTransactions'];
+			$total_amount = $this->request->data['total_amount'];
+
 			$this->Transaction->create();
 			if ($this->Transaction->save($this->request->data)) {
 				$success = true;
 			} else {
 				$success = false;
 			}
+			$payer_id = $this->request->data['payer_id'];
+			// $running_amount = 0;
+			foreach ($subtransactions as $st){
+				$user_id = $st['value'];
+				if (!$this->User->exists($user_id)){
+					$data = array();
+					list($first_name, $last_name) = explode(' ', $st['label'],1);
+
+					$this->User->create();
+					$data['id'] = $user_id;
+					$data['first_name'] = $first_name;
+					$data['last_name'] = $last_name;
+					$data['email'] = @$st['borrower_email'];
+					$this->User->save($data);
+				}
+
+				$this->Subtransaction->create();
+				$data = array();
+				$data['payer_id'] = $payer_id;
+				$data['borrower_id'] = $st['value'];
+				$data['amount'] = $st['amount'];
+				$data['transaction_id'] = $this->Transaction->id;
+				$data['accepted'] = 1;
+				$this->Subtransaction->save($data);
+
+				// $running_amount += $st['amount'];
+
+			}
+			// Save a transaction to own your self!
+			// $this->Subtransaction->create();
+			// $data = array();
+			// $data['payer_id'] = $payer_id;
+			// $data['borrower_id'] = $payer_id;
+			// $data['amount'] = $total_amount - $running_amount;
+			// $data['transaction_id'] = $this->Transaction->id;
+			// $data['accepted'] = 1;
+			// $this->Subtransaction->save($data);
+
 
 			$invalidFields = $this->Transaction->invalidFields();
 
 	        $this->set(array(
 	            'invalidFields' => $invalidFields,
 	            'success' => $success,
+	            // 'st' => $subtransactions,
 	            '_serialize' => array('invalidFields', 'success')
 	        ));
 		}
